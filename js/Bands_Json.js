4c6ac00116d36d638ed50f89b0593e5c27cff8f0
adridlv <adriandlv53@gmail.com>
2016-07-12 14:13:43 +0200

    falta CSS

--- js/Bands_Json.js
+++ js/Bands_Json.js
@@ -3,55 +3,63 @@
     "id":1,"name": "The Beatles",
     "image":"http://static.stereogum.com/uploads/2010/08/The-Beatles-PR.jpg",
     "decade":1960,
-    "songs": ["Penny Lane","Strawberry Fields Forever","Hey Jude!"]
+    "songs": ["Penny Lane","Strawberry Fields Forever","Hey Jude!"],
+    "background": "https://userscontent2.emaze.com/images/20aab8e3-6e98-429e-a577-c57d019c58a5/8aafd778dc406387bc10826906436779.png"
   },
   {
     "id":2,
     "name": "Black Sabbath",
     "image":"https://upload.wikimedia.org/wikipedia/commons/4/42/Sabs.jpg",
     "decade":1970,
-    "songs":["Paranoid","Planet Caravan","Iron Man","War Pigs"]
+    "songs":["Paranoid","Planet Caravan","Iron Man","War Pigs"],
+    "background": "http://assets.rollingstone.com/assets/2015/list/readers-poll-the-10-best-black-sabbath-deep-cuts-20151028/214468/large_rect/1445988919/1401x788-GettyImages-94171756.jpg"
   },
   {
     "id":3,
     "name": "Metallica",
     "image":"http://www.billboard.com/files/styles/article_main_image/public/media/Metallica-1985-group-billboard-650.jpg",
     "decade":1980,
-    "songs":["Seek and Destroy","One","Anesthesia Pulling Teeth"]
+    "songs":["Seek and Destroy","One","Anesthesia Pulling Teeth"],
+    "background": "http://images3.alphacoders.com/282/282245.jpg"
   },
   {
     "id":4,
     "name": "Nirvana",
     "image":"http://www.indiehoy.com/wp-content/uploads/2016/03/nirvana.jpg",
     "decade":1990,
-    "songs":["Smells like teen Spirit","Lithium"]
+    "songs":["Smells like teen Spirit","Lithium"],
+    "background": "http://www.1047hit.com/sites/default/files/nirva.jpg"
   },
   {
     "id":5,
     "name": "Kinks",
     "image":"http://i.dailymail.co.uk/i/pix/2014/10/24/1414109764217_wps_36_The_Kinks_Dave_Davies_Mic.jpg",
     "decade":1960,
-    "songs":["Waterloo Sunset","Village Green Preservation Society"]
+    "songs":["Waterloo Sunset","Village Green Preservation Society"],
+    "background": "http://static.stereogum.com/uploads/2015/05/The-Kinks.jpg"
   },
   {
     "id":6,
     "name": "The Who",
     "image":"http://assets.rollingstone.com/assets/2014/article/the-who-record-be-lucky-their-first-new-song-in-eight-years-20140925/169785/large_rect/1411659364/1401x788-154421126.jpg",
     "decade":1960,
-    "songs":["My Generation","Baba O'Riley"]
+    "songs":["My Generation","Baba O'Riley"],
+    "background": "http://www.perronegrorocks.com/wp-content/uploads/2016/06/hp_news_1459341229.jpg"
   },
   {
     "id":7,
     "name": "Green Day",
     "image":"http://qsrock.com/wp-content/uploads/2016/05/green-day.jpg",
     "decade":1990,
-    "songs":["BasketCase","BurnOut"]
+    "songs":["BasketCase","BurnOut"],
+    "background": "http://www.greenday.com/sites/g/files/g2000002086/f/styles/photo_detail_large/public/201304/Topline_Imagev2.jpg?itok=YO0cwmaZ"
   },
   {
     "id":8,
     "name": "Iron Maiden",
     "image":"http://img.thesun.co.uk/aidemitlum/archive/02738/ironmaiden_land8_2738052a.jpg",
     "decade":1980,
-    "songs":["Fear of the Dark","The Trooper"]
+    "songs":["Fear of the Dark","The Trooper"],
+    "background": "http://www.nacion.com/ocio/musica/Iron-Maiden-Costa-Rica_LNCIMA20160308_0170_1.jpg"
   }
 ]
--- js/app.js
+++ js/app.js
@@ -1,21 +1,58 @@
-var app = angular.module ("bandApp", []);
+var app = angular.module("bandApp",['ngRoute']);
 
-app.controller("groupManager", ['$scope','$http', function($scope, http){
+app.config(['$routeProvider',function($routeProvider) {
+	$routeProvider
+	.when("/",{
+		templateUrl: "views/groups.html",
+		controller: "groupManager"
+	})
+	.when("/artist/:groupName",{
+		templateUrl: "views/artist.html",
+		controller: "artistManager"
+	})
+	.otherwise({
+		redirectTo: "/"
+	})
+}]);
+
+app.controller("groupManager", ['$scope','$http', function($scope, $http){
 	$http.get("js/Bands_Json.js").success (function (data){
-        $scope.groups = data;
-    });
-}])
+		$scope.groups = data;
+	});
+}]);
+
+app.controller("artistManager", ['$scope','$http', "$routeParams", function($scope, $http, $routeParams){
+	$scope.nameFilter = $routeParams.groupName;
+
+	$http.get("js/Bands_Json.js").success (function (data){
+		$scope.groups = data;
+	});
+}]);
 
 app.directive("appHeader", function(){
 	return {
 		restrict: "E",
 		templateUrl: "views/headerView.html"
 	}
-})
+});
 
 app.directive("appFooter", function(){
 	return {
 		restrict: "E",
 		templateUrl: "views/footerView.html"
+	}
+});
+
+app.directive("artElement", function(){
+	return{
+		restrict: "E",
+		templateUrl: "views/group.html"
+	}
+});
+
+app.directive("artBackground", function(){
+	return{
+		restrict: "E",
+		templateUrl: "views/artistBackground.html"
 	}
 })
